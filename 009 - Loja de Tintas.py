cdc53951d904868f22f747fd6c40df4d249ed873
Dorgival Filho <45520261+DorgyFilho@users.noreply.github.com>
2019-03-21 09:39:25 -0300

    Update 009 - Loja de Tintas.py

--- 009 - Loja de Tintas.py
+++ 009 - Loja de Tintas.py
@@ -1,13 +1,17 @@
 #009 - Loja de Tintas
 
 metros = int(input('Metros a serem pintados: '))
-lts = metros/3
+lts = metros//3
 
 precoLata = 80
 capLata = 18
 
-qtdLata = lts/capLata
-qtdLata = round(qtdLata)
+qtdLata = lts//capLata
+resto = lts % 18
+if resto > 0:
+    qtdLata += 1
 totalCompra = qtdLata*precoLata
 
 print(f'Quantidade de latas adquiridas: {qtdLata}\nValor total da compra: R${totalCompra:.2f}')
+
+

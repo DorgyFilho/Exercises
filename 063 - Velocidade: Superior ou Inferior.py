60265616ed32a2781a1bb6783bedc93c72e9ca32
Dorgival Filho <45520261+DorgyFilho@users.noreply.github.com>
2019-03-22 08:52:20 -0300

    Update 063 - Velocidade: Superior ou Inferior.py

--- 063 - Velocidade: Superior ou Inferior.py
+++ 063 - Velocidade: Superior ou Inferior.py
@@ -5,5 +5,7 @@ duraViagem = float(input('Tempo em horas: '))
 vMedia = dist/duraViagem
 if vMedia > 110:
     print(f'Velocidade: {vMedia:.0f} Km/h --> A velocidade foi superior a 110 Km/h')
+elif vMedia == 110:
+    print('Você está no limite de velocidade.')
 else:
     print('A sua velocidade ficou abaixo dos 110 Km/h.')

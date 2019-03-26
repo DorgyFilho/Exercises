6ce001798ee75c785201bf77bb67870d4140557d
Dorgival Filho <45520261+DorgyFilho@users.noreply.github.com>
2019-03-26 11:50:31 -0300

    Uploaded in 2019-03-21 / Updated in 2019-03-26

--- 072 - Os 15 Primeiros Números Primos.py
+++ 072 - Os 15 Primeiros Números Primos.py
@@ -1,9 +1,15 @@
 #072 - Os 15 Primeiros Números Primos
 
+menor = int(input('Digite um número: '))
+maior = int(input('Digite outro número: '))
 count = 0
-for i in range(2,100):
-    if i % 2 == 1:
-        count += 1
-        if count == 16:
-            break
-        print(i, end=' ')
+for num in range(menor, maior+1):
+    if num > 1:
+        for i in range(2,num):
+            if num % i == 0:
+                break
+        else:
+            print(f'{num} é primo')
+            count += 1
+            if count == 15:
+                break

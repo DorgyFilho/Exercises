dacdb3be421fa05c42d9657ead2296ad0f414760
Dorgival Filho <45520261+DorgyFilho@users.noreply.github.com>
2019-04-08 05:04:44 -0300

    2019-04-07

--- Revisões - UFRPE/Lista #2 - Strings/003 - Sobrenome, Nome.py
+++ Revisões - UFRPE/Lista #2 - Strings/003 - Sobrenome, Nome.py
@@ -4,7 +4,7 @@ nCom = input('Nome Completo: ')
 nome = nCom.split()
 qtde = len(nome)
 
-if qtde >= 2:  #Sobrenome, Nome
+if qtde == 2:  #Sobrenome, Nome
     print(f'{nome[-1]}, {nome[0]}')
 elif qtde > 2:
     print(f'{nome[qtde-1]}, {nome[0]}')

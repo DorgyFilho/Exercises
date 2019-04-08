f3ace1cebcfa46d974704932850039a6240c9be4
Dorgival Filho <45520261+DorgyFilho@users.noreply.github.com>
2019-04-08 05:10:16 -0300

    2019-04-07

--- Revisões - UFRPE/Lista #2 - Strings/009 - Referência Acadêmica.py
+++ Revisões - UFRPE/Lista #2 - Strings/009 - Referência Acadêmica.py
@@ -6,7 +6,10 @@ nome = nCom.split()
 
 qtde = len(nome)
 
-if qtde <= 2: #Sobrenome, nome
+if qtde == 2: #Sobrenome, nome
     print(f'{nome[-1]}, {nome[0][0]}')
 elif qtde > 2: 
     print(f'{nome[qtde-1]}, {nome[0][0]}.{nome[-2][0]}')
+else:
+    print(f'{nCom}')
+    
